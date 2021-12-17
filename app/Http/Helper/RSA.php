67a453e0f3f7a45d2692c87b8b86652bc9234159
Vicktor Desrony <vldcreation21@gmail.com>
2021-12-17 21:22:59 +0700

    feat: Fix RSA

--- app/Http/Helper/RSA.php
+++ app/Http/Helper/RSA.php
@@ -5,20 +5,20 @@ namespace App\Http\Helper;
 use phpseclib\Crypt\RSA as Crypt_RSA;
 
 class RSA { 
-    public static function encryp_rsa($publickey = 'bla-123-bla',$plaintext){
-        $rsa = new Crypt_RSA();
-        $rsa->loadKey($publickey);
-        $rsa->setEncryptionMode(2);
-        $data = $plaintext;
-        $output = $rsa->encrypt($data);
-        return base64_encode($output);
-    }
-    public static function encryp_rsa($plaintext){
+    public static function encrypt_rsa($plaintext){
         $rsa = new Crypt_RSA();
         $rsa->getPrivateKey();
         $rsa->setDecryptionMode(2);
         $data = $plaintext;
         $output = $rsa->decrypt($data);
+        return base64_encode($output);
+    }
+    public static function decrypt_rsa($publickey = 'bla-123-bla',$plaintext){
+        $rsa = new Crypt_RSA();
+        $rsa->loadKey($publickey);
+        $rsa->setEncryptionMode(2);
+        $data = $plaintext;
+        $output = $rsa->encrypt($data);
         return base64_encode($output);
     }
 }
